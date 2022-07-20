6252b191646dbc2c1ca57b8c2122db175bb1110f
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-07-20 11:13:19 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,12 @@ import React from "react";
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 
-console.log(user);
-
 function App() {
   return (
     <div>
       <NavBar />
-      <Home name={user.name} color={user.color} city={user.city}/>
-      <About bio={user.bio} links={user.links}/>
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
